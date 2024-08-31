c58207dc80b9ed85e81e6526487563576c8bca5a
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-08-31 07:46:00 +0100

    Add PaystackClient class

---
+++ src/paystack.ts
@@ -0,0 +1,13 @@
+import RestClient from "./restClient.ts";
+
+export default class PaystackClient {
+  client: RestClient;
+
+  constructor(secretKey?: string, client?: RestClient) {
+    if (client) {
+      this.client = client;
+    } else {
+      this.client = new RestClient(secretKey);
+    }
+  }
+}
