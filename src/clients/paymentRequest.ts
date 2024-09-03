ac63fce49b29611fdb9e094291d5b4d7afd6afb7
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-04 00:28:37 +0100

    Implement PaymentRequestClient

---
+++ src/clients/paymentRequest.ts
@@ -0,0 +1,47 @@
+import RestClient, { HTTPMethod } from "../restClient.ts";
+import { CreatePaymentRequestPayload, GetPaymentRequestsOptions } from "../types/clients/paymentRequest.ts";
+
+
+export default class PaymentRequestClient {
+  client: RestClient;
+
+  constructor(secretKey?: string, client?: RestClient) {
+    if (client) {
+      this.client = client;
+    } else {
+      this.client = new RestClient(secretKey);
+    }
+  }
+
+  create(payload: CreatePaymentRequestPayload){
+    return this.client.call('/paymentrequest', HTTPMethod.POST,payload)
+  }
+
+  getPaymentRequests(options?: GetPaymentRequestsOptions){
+    return this.client.call('/paymentrequest', HTTPMethod.POST, null, options)
+  }
+
+  getPaymentRequest(idOrCode: string){
+    return this.client.call(`/paymentrequest/${idOrCode}`, HTTPMethod.GET)
+  }
+
+  verify(code:string){
+    return this.client.call(`/paymentrequest/verify/${code}`, HTTPMethod.GET)
+  }
+
+  sendNotification(idOrCode: string){
+    return this.client.call(`/paymentrequest/notify/${idOrCode}`, HTTPMethod.POST)
+  }
+
+  getTotal(){
+    return this.client.call('/paymentrequest/totals',HTTPMethod.GET)
+  }
+
+  finalize(idOrCode: string){
+    return this.client.call(`/paymentrequest/finalize/${idOrCode}`, HTTPMethod.GET)
+  }
+
+  update(idOrCode: string, payload: UpdatePaymentRequestPayload){
+    return this.client.call(`/paymentrequest/${idOrCode}`, HTTPMethod.PUT, payload)
+  }
+}
