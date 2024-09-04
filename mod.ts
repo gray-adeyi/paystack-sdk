57651e1c3720402580753900804dbf9afaebd80e
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-04 13:14:38 +0100

    Export public api

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+export { default as PaystackClient } from "./src/paystack.ts";
+export * from "./src/types/clients/index.ts";
+export * from "./src/types/global.ts";
+export * from "./src/enums.ts";
