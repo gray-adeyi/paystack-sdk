666264ea771a0cd981c9e0b130739201370b5482
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-04 13:17:42 +0100

    Add applePay test

---
+++ tests/applePay_test.ts
@@ -0,0 +1,22 @@
+import { beforeAll, describe, it } from "@std/testing/bdd";
+import { PaystackClient } from "../mod.ts";
+import { load } from "@std/dotenv";
+import { assertEquals } from "@std/assert";
+
+describe("PaystackClient.applePay", () => {
+  let client: PaystackClient;
+
+  beforeAll(async () => {
+    await load({ envPath: "./.env", export: true });
+    client = new PaystackClient();
+  });
+
+  it("PaystackClient.applePay.registerDomain", () => {});
+  it("PaystackClient.applePay.getDomains", async () => {
+    const response = await client.applePay.getDomains();
+    assertEquals(response.statusCode, 200);
+    assertEquals(response.message,'Apple Pay registered domains retrieved')
+    assertEquals(response.data,{domainNames:[]})
+  });
+  it("PaystackClient.applePay.unregisterDomain", () => {});
+});
