ee07ebd02f503e7ab815b3457afcb96dc91fca85
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-05 12:37:08 +0100

    Document CustomerClient

--- src/clients/customer.ts
+++ src/clients/customer.ts
@@ -6,10 +6,23 @@ import type {
   UpdateCustomerPayload,
   ValidatePayload,
 } from "../types/clients/customer.ts";
+import type { PaystackResponse } from "../types/global.ts";
 
+/**
+ * CustomerClient provides methods that lets you interface with Paystack's
+ * Customers API which allows you to create and manage customers in your integration.
+ * https://paystack.com/docs/api/customer/
+ */
 export default class CustomerClient {
-  client: RestClient;
+  private client: RestClient;
 
+  /**
+   * @constructor Instantiate a BulkChargeClient
+   *
+   * @param secretKey - Your paystack integration secret key.
+   * @param client - A custom rest client to use for making api calls to paystack's instead
+   * of creating a new one with the secretKey
+   */
   constructor(secretKey?: string, client?: RestClient) {
     if (client) {
       this.client = client;
@@ -18,23 +31,64 @@ export default class CustomerClient {
     }
   }
 
-  create(payload: CreateCustomerPayload) {
+  /**
+   * Create a customer on your integration.
+   *
+   * @param payload : Is a {@link CreateCustomerPayload} containing the customer's
+   * information.
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  create(payload: CreateCustomerPayload): Promise<PaystackResponse> {
     return this.client.call("/customer/", HTTPMethod.POST, payload);
   }
 
-  getCustomers(options?: GetCustomersOptions) {
+  /**
+   * Retrieve customers available on your integration.
+   *
+   * @param options : {@link GetCustomersOptions} let's you customize the data in the
+   * response to be returned.
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  getCustomers(options?: GetCustomersOptions): Promise<PaystackResponse> {
     return this.client.call("/customer/", HTTPMethod.GET, null, options);
   }
 
-  getCustomer(emailOrCode: string) {
+  /**
+   * Get details of a customer in your integration.
+   *
+   * @param emailOrCode : An email or customer code for the customer you want to fetch
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  getCustomer(emailOrCode: string): Promise<PaystackResponse> {
     return this.client.call(`/customer/${emailOrCode}`, HTTPMethod.GET);
   }
 
-  update(code: string, payload: UpdateCustomerPayload) {
+  /**
+   * Update a customer's details on your integration
+   *
+   * @param code : The customer's code
+   * @param payload {@link UpdateCustomerPayload} is the data used to update the
+   * customer's details
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  update(
+    code: string,
+    payload: UpdateCustomerPayload,
+  ): Promise<PaystackResponse> {
     return this.client.call(`/customer/${code}`, HTTPMethod.PUT, payload);
   }
 
-  validate(emailOrCode: string, payload: ValidatePayload) {
+  /**
+   * Validate a customer's identity
+   *
+   * @param emailOrCode : The email or code of the customer to be validated
+   * @param payload : {@link ValidatePayload} is the data used for the validation.
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  validate(
+    emailOrCode: string,
+    payload: ValidatePayload,
+  ): Promise<PaystackResponse> {
     return this.client.call(
       `/customer/${emailOrCode}/identification`,
       HTTPMethod.POST,
@@ -42,14 +96,27 @@ export default class CustomerClient {
     );
   }
 
-  flag(customer: string, riskAction: RiskAction) {
+  /**
+   * Whitelist or blacklist a customer on your integration.
+   *
+   * @param emailOrCode : The customer's email address or code.
+   * @param riskAction : One of the possible risk actions from the RiskAction enum e.g `RiskAction.DEFAULT`
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  flag(emailOrCode: string, riskAction: RiskAction): Promise<PaystackResponse> {
     return this.client.call("/customer/set_risk_action", HTTPMethod.POST, {
-      customer,
+      customer: emailOrCode,
       riskAction,
     });
   }
 
-  deactivate(authCode: string) {
+  /**
+   * Deactivate an authorization when the card needs to be forgotten
+   *
+   * @param authCode : Authorization code to be deactivated.
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  deactivate(authCode: string): Promise<PaystackResponse> {
     return this.client.call(
       "/customer/deactivate_authorization",
       HTTPMethod.POST,
