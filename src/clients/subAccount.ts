28cf38c51aac4b9e702d8ed311e4fcd22af77c3f
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-05 22:18:07 +0100

    Document SubAccountClient

--- src/clients/subAccount.ts
+++ src/clients/subAccount.ts
@@ -5,9 +5,22 @@ import type {
   UpdateSubAccountPayload,
 } from "../types/clients/subAccount.ts";
 
+/**
+ * SubAccountClient provides methods that lets you interface with Paystack's
+ * Subaccounts API which allows you to create and manage subaccounts on your integration.
+ * Subaccounts can be used to split payment between two accounts
+ * (your main account and a sub account). https://paystack.com/docs/api/subaccount/
+ */
 export default class SubAccountClient {
-  client: RestClient;
+  private client: RestClient;
 
+  /**
+   * @constructor Instantiate a SubAccountClient
+   *
+   * @param secretKey - Your paystack integration secret key.
+   * @param client - A custom rest client to use for making api calls to paystack's instead
+   * of creating a new one with the secretKey
+   */
   constructor(secretKey?: string, client?: RestClient) {
     if (client) {
       this.client = client;
@@ -16,18 +29,46 @@ export default class SubAccountClient {
     }
   }
 
+  /**
+   * Create a subacount on your integration.
+   *
+   * @param payload : {@link CreateSubAccountPayload} is the data used to
+   * create a sub account.
+   * @returns A promise containing a {@link PaystackResponse}
+   */
   create(payload: CreateSubAccountPayload) {
     return this.client.call("/subaccount", HTTPMethod.POST, payload);
   }
 
+  /**
+   * Fetch subaccounts available on your integration.
+   *
+   * @param options : {@link GetSubAccountsOptions} lets you customize the
+   * data that is returned in the response.
+   * @returns A promise containing a {@link PaystackResponse}
+   */
   getSubAccounts(options?: GetSubAccountsOptions) {
     return this.client.call("/subaccount", HTTPMethod.GET, null, options);
   }
 
+  /**
+   * Get details of a subaccount on your integration.
+   *
+   * @param idOrCode : The subaccount ``ID`` or ``code`` you want to fetch
+   * @returns A promise containing a {@link PaystackResponse}
+   */
   getSubAccount(idOrCode: string) {
     return this.client.call(`/subaccount/${idOrCode}`, HTTPMethod.GET);
   }
 
+  /**
+   * Update a subaccount details on your integration.
+   *
+   * @param idOrCode : The subaccount ``ID`` or ``code`` you want to update
+   * @param payload : {@link UpdateSubAccountPayload} is the data used to update
+   * the subaccount
+   * @returns A promise containing a {@link PaystackResponse}
+   */
   update(idOrCode: string, payload: UpdateSubAccountPayload) {
     return this.client.call(`/subaccount/${idOrCode}`, HTTPMethod.PUT, payload);
   }
