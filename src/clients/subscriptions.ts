436df21cdd68432ca3cdd3d82d5a2e421b87dbdb
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-06 09:36:26 +0100

    Document SubscriptionClient

--- src/clients/subscriptions.ts
+++ src/clients/subscriptions.ts
@@ -3,10 +3,24 @@ import type {
   CreateSubscriptionPayload,
   GetSubscriptionsOptions,
 } from "../types/clients/subscriptions.ts";
+import type { PaystackResponse } from "../types/global.ts";
 
+/**
+ * SubscriptionClient provides methods that lets you interface with Paystack's
+ * Subscriptions API allows you to create and manage recurring
+ * payment on your integration.
+ * https://paystack.com/docs/api/subscription/
+ */
 export default class SubscriptionClient {
-  client: RestClient;
+  private client: RestClient;
 
+  /**
+   * @constructor Instantiate a SubscriptionClient
+   *
+   * @param secretKey - Your paystack integration secret key.
+   * @param client - A custom rest client to use for making api calls to paystack's instead
+   * of creating a new one with the secretKey
+   */
   constructor(secretKey?: string, client?: RestClient) {
     if (client) {
       this.client = client;
@@ -15,40 +29,88 @@ export default class SubscriptionClient {
     }
   }
 
-  create(payload: CreateSubscriptionPayload) {
+  /**
+   * Create a subscription on your integration.
+   *
+   * @param payload : {@link CreateSubscriptionPayload} is the data sent to paystack
+   * to create a subscription
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  create(payload: CreateSubscriptionPayload): Promise<PaystackResponse> {
     return this.client.call("/subscription", HTTPMethod.POST, payload);
   }
 
-  getSubscriptions(options?: GetSubscriptionsOptions) {
+  /**
+   * Fetch subscriptions available on your integration.
+   *
+   * @param options: {@link GetSubscriptionsOptions} lets you customize the data
+   * that is returned in the response.
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  getSubscriptions(
+    options?: GetSubscriptionsOptions,
+  ): Promise<PaystackResponse> {
     return this.client.call("/subscription", HTTPMethod.GET, null, options);
   }
 
-  getSubscription(idOrCode: string) {
+  /**
+   * Fetch details of a subscription on your integration.
+   *
+   * @param idOrCode: The subscription ``ID`` or ``code`` you want to fetch
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  getSubscription(idOrCode: string): Promise<PaystackResponse> {
     return this.client.call(`/subscription/${idOrCode}`, HTTPMethod.GET);
   }
 
-  enable(code: string, token: string) {
+  /**
+   * Enable a subscription on your integration
+   *
+   * @param code: Subscription code
+   * @param token: Email token
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  enable(code: string, token: string): Promise<PaystackResponse> {
     return this.client.call("/subscription/enable", HTTPMethod.POST, {
       code,
       token,
     });
   }
 
-  disable(code: string, token: string) {
+  /**
+   * Disable a subscription on your integration
+   *
+   * @param code: Subscription code
+   * @param token: Email token
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  disable(code: string, token: string): Promise<PaystackResponse> {
     return this.client.call("/subscription/disable", HTTPMethod.POST, {
       code,
       token,
     });
   }
 
-  getUpdateLink(code: string) {
+  /**
+   * Generate a link for updating the card on a subscription
+   *
+   * @param code: Subscription code
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  getUpdateLink(code: string): Promise<PaystackResponse> {
     return this.client.call(
       `/subscription/${code}/manage/link`,
       HTTPMethod.GET,
     );
   }
 
-  sendUpdateLink(code: string) {
+  /**
+   * Email a customer a link for updating the card on their subscription
+   *
+   * @param code : Subscription code
+   * @returns A promise containing a {@link PaystackResponse}
+   */
+  sendUpdateLink(code: string): Promise<PaystackResponse> {
     return this.client.call(
       `/subscription/${code}/manage/email/`,
       HTTPMethod.POST,
