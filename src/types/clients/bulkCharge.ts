4d75482d8ea6c6fef3939c5fbf0a20dffd23956e
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-06 18:16:10 +0100

    Document bulk charge types

--- src/types/clients/bulkCharge.ts
+++ src/types/clients/bulkCharge.ts
@@ -1,14 +1,31 @@
 import type { Status } from "../../enums.ts";
 import type { PaginationAndDateFilterOptions } from "../global.ts";
 
+/**
+ * A representation of a unit of a bulk charge
+ */
 export type BulkChargeInstruction = {
+  /** The authorization code of the customer you want to charge. */
   readonly authorization: string;
+  /** The amount you want to charge. */
   readonly amount: number;
+  /** The transaction reference. */
   readonly reference: string;
 };
 
+/**
+ * Lets you customize how the batches are retrieved by filtering
+ * by date or adjusting how much data should be returned or which
+ * sets of data to be returned by pagination.
+ */
 export type GetBatchesOptions = PaginationAndDateFilterOptions;
 
+/**
+ * Lets you customize how the batches are retrieved by filtering
+ * by date or adjusting how much data should be returned or which
+ * sets of data to be returned by pagination.
+ */
 export type GetChargesInBatchOptions = PaginationAndDateFilterOptions & {
+  /** Lets you filter by the charge status */
   readonly status: Status;
 };
