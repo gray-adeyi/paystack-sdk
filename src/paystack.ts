f47d1dc79e7e093ce3108d4960c80d3a117a33a8
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-18 11:43:45 +0100

    Document PaystackClient

--- src/paystack.ts
+++ src/paystack.ts
@@ -23,30 +23,190 @@ import TransferControlClient from "./clients/transfersControl.ts";
 import VerificationClient from "./clients/verification.ts";
 import RestClient from "./restClient.ts";
 
+/**
+ * PaystackClient has other dedicated clients bound to it. This provides a convenience for interacting
+ * with all of paystack's endpoints in your JS/TS project. As stated above, it has other dedicated clients bound to it as
+ * class properties, therefore, after creating an instance of PaystackClient. You can access the associated
+ * methods of each dedicated client via its property name. e.g.to get supported banks, you access the `getBanks`
+ * method of the `MiscellaneousClient` via the `miscellaneous` property. i.e. `paystackClientInstance.miscellaneous.getBanks`.
+ * To create a new customer on your integration, you access the `create` method of the `CustomerClient` via the `customers`
+ * property. i.e. `paystackClientInstance.customers.create`.
+ */
 export default class PaystackClient {
   private readonly client: RestClient;
+  /**
+   * applePay is a binding to the {@link ApplePayClient} that lets you interact with endpoints 
+   * related to paystack Apple Pay resource that lets you register your application's 
+   * top-level domain or subdomain. It provides methods that maps to all the endpoints 
+   * in the Paystack's ApplePay API reference.
+   * @see https://paystack.com/docs/api/apple-pay/
+   */
   readonly applePay: ApplePayClient;
+  /**
+   * bulkCharges is a binding to the {@link BulkChargeClient} that lets you interact with endpoints 
+   * related to paystack bulk Charges resource that lets you create and manage multiple 
+   * recurring payments from your Customers. It provides methods that maps to all the
+   * endpoints in the Paystack's Bulk Charge API reference.
+   * @see https://paystack.com/docs/api/bulk-charge/
+   */ 
   readonly bulkCharges: BulkChargeClient;
+  /**
+   * charge is a binding to the {@link ChargeClient} that lets you interact with endpoints related 
+   * to paystack charge resource that lets you configure a payment channel of your choice 
+   * when initiating a payment. It provides methods that maps to all the endpoints in the
+   * Paystack's Charge API reference. @see https://paystack.com/docs/api/charge/
+   */ 
   readonly charge: ChargeClient;
+  /**
+   * integration is a binding to the {@link IntegrationClient} that lets you interact with endpoints 
+   * related to paystack Integration resource that lets you manage some settings on your Integration.
+   * It provides methods that maps to all the endpoints in the Paystack's Integration API reference.
+   * @see https://paystack.com/docs/api/integration/
+   */ 
   readonly integration: IntegrationClient;
+  /**
+   * customers is a binding to the {@link CustomerClient} that lets you interact with endpoints related to 
+   * Paystack's Customer resource that allows you to create and manage Customers on your Integration.
+   * It provides methods that maps to all the endpoints in the Paystack's Customer API reference.
+   * @see https://paystack.com/docs/api/customer/
+   */
   readonly customers: CustomerClient;
+  /**
+   * dedicatedAccounts is a binding to the {@link DedicatedAccountClient} that lets you interact with endpoints 
+   * related to paystack dedicated virtual account resource that enables Nigerian merchants to 
+   * manage unique payment accounts of their Customers. It provides methods that maps to all the
+   * endpoints in the Paystack's Dedicated Virtual Accounts API reference.
+   * @see https://paystack.com/docs/api/dedicated-virtual-account/
+   */
   readonly dedicatedAccounts: DedicatedAccountClient;
+  /**
+   * disputes is a binding to the {@link DisputeClient} that lets you interact with endpoint 
+   * related to paystack dispute resource that lets you manage transaction Disputes on 
+   * your Integration. It provides methods that maps to all the endpoints in the Paystack's
+   * Dispute API reference. @see https://paystack.com/docs/api/dispute/
+   */ 
   readonly disputes: DisputeClient;
+  /**
+   * paymentRequests is a binding to the {@link PaymentRequestClient} that lets you interacts with endpoints 
+   * related to paystack payment request resource that lets you manage requests for payment of goods 
+   * and services. It provides methods that maps to all the endpoints in the Paystack's Payment
+   * Request API reference.
+   * @see https://paystack.com/docs/api/payment-request/
+   */
   readonly paymentRequests: PaymentRequestClient;
+  /**
+   * miscellaneous is a binding to the {@link MiscellaneousClient} that lets you interact with endpoints 
+   * related to paystack Miscellaneous resource that provides information that is relevant to 
+   * other client methods. It provides methods that maps to all the endpoints in the Paystack's
+   * Miscellaneous API reference. @see https://paystack.com/docs/api/miscellaneous/
+   */
   readonly miscellaneous: MiscellaneousClient;
+  /**
+   * paymentPages is a binding to the {@link PaymentPageClient} that lets you interact with endpoints 
+   * related to paystack payment page resource that lets you provide a quick and secure way 
+   * to collect payment for Products. It provides methods that maps to all the endpoints
+   * in the Paystack's Payment Pages API reference
+   * @see https://paystack.com/docs/api/page/
+   */ 
   readonly paymentPages: PaymentPageClient;
+  /**
+   * plans is a binding to the {@link PlanClient} that lets you interact with endpoints related 
+   * to paystack plan resource that lets you create and manage installment payment 
+   * options on your Integration. It provides methods that maps to all the endpoints
+   * in the Paystack's Plans API reference.
+   * @see https://paystack.com/docs/api/plan/
+   */
   readonly plans: PlanClient;
+  /**
+   * products is a binding to the {@link ProductClient} that lets you interact with endpoints 
+   * related to paystack product resource that allows you to create and manage inventories 
+   * on your Integration. It provides methods that maps to all the endpoint in the Paystack's
+   * Product API reference.
+   * @see https://paystack.com/docs/api/product/
+   */ 
   readonly products: ProductClient;
+  /**
+   * refunds is a binding to the {@link RefundClient} that lets you interact with endpoints related 
+   * to paystack refund resource that lets you create and manage transaction Refunds.
+   * It provides methods that maps to all the endpoint in the Paystack's Refund API
+   * reference. @see https://paystack.com/docs/api/refund/
+   */
   readonly refunds: RefundClient;
+  /**
+   * settlements is a binding to the {@link SettlementClient} that lets you interact with endpoints 
+   * related to paystack settlement resource that lets you gain insights into payouts made 
+   * by Paystack to your bank account. It provides methods that maps to all endpoints in the
+   * Paystack's Settlement API reference.
+   * @see https://paystack.com/docs/api/settlement/
+   */ 
   readonly settlements: SettlementClient;
+  /**
+   * splits is a binding to the {@link TransactionSplitClient} that lets you interact with endpoints
+   * related to Paystack's Transaction Split resource that allows you to split the settlement
+   * for a transaction across a payout account, and one or more subaccounts. It provides
+   * methods that maps to all the endpoints in the Paystack's Transaction Split API reference.
+   * @see https://paystack.com/docs/api/split/
+   */
   readonly splits: TransactionSplitClient;
+  /**
+   * subAccounts is a biding to the {@link SubAccountClient} that lets you interact with endpoints 
+   * related to paystack subaccount resource that lets you create and manage subaccounts 
+   * on your Integration. Subaccounts can be used to split payment between two accounts 
+   * (your main account and a subaccount). It provides methods that maps to all the
+   * endpoints in the Paystack's Subaccount API reference.
+   * @see https://paystack.com/docs/api/subaccount/
+   */ 
   readonly subAccounts: SubAccountClient;
+  /**
+   * subscriptions is a binding to the {@link SubscriptionClient} lets you interact with endpoints 
+   * related to paystack subscription resource that lets you create and manage recurring 
+   * payment on your Integration. It provides methods that maps to all the endpoints in
+   * the Paystack's Subscription API reference.
+   * @see https://paystack.com/docs/api/subscription/
+   */ 
   readonly subscriptions: SubscriptionClient;
+  /**
+   * terminals is a binding to the {@link TerminalClient} that let you interact with endpoints 
+   * related to Paystack's Terminal resource that allows you to build delightful in-person 
+   * payment experiences. It provides methods that maps to all the endpoints in Paystack's
+   * Terminals API reference. @see https://paystack.com/docs/api/terminal/
+   */
   readonly terminals: TerminalClient;
+  /**
+   * transactions is a binding to the {@link TransactionClient} that lets you interact with endpoints
+   * related to Paystack's Transaction resource that allows you to create and manage payments
+   * on your Integration. It provides methods that maps to all the endpoints in paystack's
+   * Transaction API reference. @see https://paystack.com/docs/api/transaction/
+   */
   readonly transactions: TransactionClient;
+  /**
+   * transferRecipients is a binding to the {@link TransferRecipientClient} that lets you interact 
+   * with endpoints related to paystack transfer recipient resource that lets you create 
+   * and manage beneficiaries that you send money to. It provides methods that maps to all
+   * the endpoints in Paystack's Transfer Recipients API reference.
+   * @see https://paystack.com/docs/api/transfer-recipient/
+   */
   readonly transferRecipients: TransferRecipientClient;
+  /**
+   * transfers is a binding to the {@link TransferClient} that lets you interact with endpoints 
+   * related to paystack transfer resource that lets you automate sending money to your
+   * Customers. It provides methods that maps to all the endpoints in Paystack's
+   * Transfers API reference. @see https://paystack.com/docs/api/transfer/
+   */
   readonly transfers: TransferClient;
+  /**
+   * transferControl is a binding to the {@link TransferControlClient} that lets you interact with
+   * endpoints related to paystack transfer control resource that lets you manage settings
+   * of your Transfers. It provides methods that maps to all endpoints in the Paystack's
+   * Transfer Control API reference. @see https://paystack.com/docs/api/transfer-control/
+   */
   readonly transferControl: TransferControlClient;
+  /**
+   * verification is a binding to the {@link VerificationClient} that lets you interact with 
+   * endpoints related to paystack Verification resource that allows you to perform 
+   * KYC processes. It provides methods that maps to all endpoints in the Paystack's
+   * Verification API reference. @see https://paystack.com/docs/api/verification/
+   */ 
   readonly verification: VerificationClient;
 
   constructor(secretKey?: string, client?: RestClient) {
