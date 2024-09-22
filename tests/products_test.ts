fb507656a1182a0b2b20a9c345ec63e49fb97d0c
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-22 18:42:25 +0100

    Test ProductClient

--- tests/products_test.ts
+++ tests/products_test.ts
@@ -1,19 +1,47 @@
 import { beforeAll, describe, it } from "@std/testing/bdd";
-import { PaystackClient } from "../mod.ts";
+import { Currency, PaystackClient, PaystackClientError, type CreateProductPayload, type UpdateProductPayload } from "../mod.ts";
 import { load } from "@std/dotenv";
+import { assertEquals } from "@std/assert/equals";
+import { HttpStatusCode } from "axios";
+import { assertRejects } from "@std/assert/rejects";
+import { assertInstanceOf } from "@std/assert/instance-of";
 
 describe("PaystackClient.products", () => {
   let client: PaystackClient;
 
   beforeAll(async () => {
-    await load({ envPath: "./.env", export: true });
+    await load({ envPath: "../.env", export: true });
+    client = new PaystackClient()
   });
 
-  it("PaystackClient.products.create", () => {});
+  it("PaystackClient.products.create",async () => {
+    const payload: CreateProductPayload = {
+      name: 'Test product',
+      description: 'A test product',
+      price: 300_000_000,
+      currency: Currency.NGN
+    }
+    const response =await client.products.create(payload)
+    assertEquals(response.statusCode, HttpStatusCode.Created)
+    assertEquals(response.message,'Product successfully created')
+  });
 
-  it("PaystackClient.products.getproducts", () => {});
+  it("PaystackClient.products.getProducts", async () => {
+    const response = await client.products.getProducts({perPage:1})
+    assertEquals(response.statusCode, HttpStatusCode.Ok)
+    assertEquals(response.message,'Products retrieved')
+  });
 
-  it("PaystackClient.products.getProduct", () => {});
+  it("PaystackClient.products.getProduct", async () => {
+    const response = await client.products.getProduct('1717501')
+    assertEquals(response.statusCode, HttpStatusCode.Ok)
+    assertEquals(response.message,'Product retrieved')
+  });
 
-  it("PaystackClient.products.update", () => {});
+  it("PaystackClient.products.update", async () => {
+    const payload: UpdateProductPayload = {name: 'Test product', description: 'A test product',price: 500_000_000, currency: Currency.NGN}
+    const err = await assertRejects(() => client.products.update("PROD_0tbswmrz56osyxr",payload))
+    assertInstanceOf(err,PaystackClientError)
+    assertEquals(err.status, HttpStatusCode.InternalServerError)
+  });
 });
