93d97ef7570c8cd62c2fce7b0f8389f99bd33581
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-22 18:42:11 +0100

    Test PlanClient

--- tests/plan_test.ts
+++ tests/plan_test.ts
@@ -1,19 +1,45 @@
 import { beforeAll, describe, it } from "@std/testing/bdd";
-import { PaystackClient } from "../mod.ts";
+import { Interval, PaystackClient, type CreatePlanPayload, type UpdatePlanPayload } from "../mod.ts";
 import { load } from "@std/dotenv";
+import { assertEquals } from "@std/assert/equals";
+import { HttpStatusCode } from "axios";
 
 describe("PaystackClient.plan", () => {
   let client: PaystackClient;
 
   beforeAll(async () => {
-    await load({ envPath: "./.env", export: true });
+    await load({ envPath: "../.env", export: true });
+    client = new PaystackClient()
   });
 
-  it("PaystackClient.plan.create", () => {});
+  it("PaystackClient.plan.create", async () => {
+    const payload: CreatePlanPayload = {
+      name:'@gray-adeyi/paystack-sdk Test Plan',
+      amount: 2_000_000,
+      interval: Interval.MONTHLY,
+    }
+    const response =await client.plans.create(payload)
+    assertEquals(response.statusCode, HttpStatusCode.Created)
+    assertEquals(response.message,'Plan created')
+  });
 
-  it("PaystackClient.plan.getplans", () => {});
+  it("PaystackClient.plan.getPlans", async () => {
+    const response = await client.plans.getPlans({perPage: 2})
+    assertEquals(response.statusCode, HttpStatusCode.Ok)
+    assertEquals(response.message,'Plans retrieved')
+  });
 
-  it("PaystackClient.plan.getplan", () => {});
+  it("PaystackClient.plan.getPlan", async () => {
+    const response = await client.plans.getPlan('PLN_uwqsi04fu3xcmwb')
+    assertEquals(response.statusCode, HttpStatusCode.Ok)
+    assertEquals(response.message,'Plan retrieved')
+  });
 
-  it("PaystackClient.plan.update", () => {});
+  it("PaystackClient.plan.update", async () => {
+    const payload: UpdatePlanPayload = {
+      amount: 300_000_000
+    }
+    const response = await client.plans.update('PLN_uwqsi04fu3xcmwb',payload)
+    assertEquals(response.statusCode, HttpStatusCode.Ok)
+  });
 });
