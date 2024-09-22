544009b8ab7fa788cd9db9468e846e51dc471a9e
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-22 18:41:51 +0100

    Test PaymentRequestClient

--- tests/paymentRequest_test.ts
+++ tests/paymentRequest_test.ts
@@ -1,29 +1,118 @@
 import { beforeAll, describe, it } from "@std/testing/bdd";
-import { PaystackClient } from "../mod.ts";
+import {
+  type CreatePaymentRequestPayload,
+  PaystackClient,
+  PaystackClientError,
+  type UpdatePaymentRequestPayload,
+} from "../mod.ts";
 import { load } from "@std/dotenv";
+import { assertEquals } from "@std/assert/equals";
+import { HttpStatusCode } from "axios";
+import { assertRejects } from "@std/assert/rejects";
+import { assertInstanceOf } from "@std/assert/instance-of";
 
 describe("PaystackClient.paymentRequest", () => {
   let client: PaystackClient;
 
   beforeAll(async () => {
-    await load({ envPath: "./.env", export: true });
+    await load({ envPath: "../.env", export: true });
+    client = new PaystackClient();
   });
 
-  it("PaystackClient.paymentRequest.create", () => {});
+  it("PaystackClient.paymentRequests.create", async () => {
+    const payload: CreatePaymentRequestPayload = {
+      customer: "CUS_x1hp1dli4mdo1v0",
+      amount: 1000,
+    };
+    const response = await client.paymentRequests.create(payload);
+    assertEquals(response.statusCode, HttpStatusCode.Ok),
+      assertEquals(response.message, "Payment request created");
+  });
 
-  it("PaystackClient.paymentRequest.getPaymentRequests", () => {});
+  it("PaystackClient.paymentRequests.getPaymentRequests", async () => {
+    const response = await client.paymentRequests.getPaymentRequests();
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
+    assertEquals(response.message, "Payment requests retrieved");
+  });
 
-  it("PaystackClient.paymentRequest.getPaymentRequest", () => {});
+  it("PaystackClient.paymentRequests.getPaymentRequest", async () => {
+    const response = await client.paymentRequests.getPaymentRequest(
+      "PRQ_m0pqwkxih8teah1",
+    );
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
+    assertEquals(response.message, "Payment request retrieved");
+  });
 
-  it("PaystackClient.paymentRequest.verify", () => {});
+  it("PaystackClient.paymentRequests.verify", async () => {
+    const response = await client.paymentRequests.verify("PRQ_my1uk6vldrwuj2w");
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
+    assertEquals(response.message, "Payment request retrieved");
+  });
 
-  it("PaystackClient.paymentRequest.sendNotification", () => {});
+  it("PaystackClient.paymentRequests.sendNotification", async () => {
+    const response = await client.paymentRequests.sendNotification(
+      "PRQ_my1uk6vldrwuj2w",
+    );
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
+    assertEquals(response.message, "Notification sent");
+  });
 
-  it("PaystackClient.paymentRequest.getTotal", () => {});
+  it("PaystackClient.paymentRequests.getTotal", async () => {
+    const response = await client.paymentRequests.getTotal();
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
+    assertEquals(response.message, "Payment request totals");
+  });
 
-  it("PaystackClient.paymentRequest.finalize", () => {});
+  it("PaystackClient.paymentRequests.finalize", async () => {
+    const createPayload: CreatePaymentRequestPayload = {
+      customer: "CUS_x1hp1dli4mdo1v0",
+      amount: 1000,
+    };
+    const createResponse = await client.paymentRequests.create(createPayload);
+    assertEquals(createResponse.statusCode, HttpStatusCode.Ok);
+    console.log(createResponse);
+    type DataOfInterest = { id: string; requestCode: string };
+    const err = await assertRejects(() =>
+      client.paymentRequests.finalize(
+        (createResponse.data as DataOfInterest).requestCode,
+      )
+    );
+    assertInstanceOf(err, PaystackClientError);
+    assertEquals(err.status, HttpStatusCode.NotFound);
+  });
 
-  it("PaystackClient.paymentRequest.update", () => {});
+  it("PaystackClient.paymentRequests.update", async () => {
+    const createPayload: CreatePaymentRequestPayload = {
+      customer: "CUS_x1hp1dli4mdo1v0",
+      amount: 1000,
+    };
+    const createResponse = await client.paymentRequests.create(createPayload);
+    assertEquals(createResponse.statusCode, HttpStatusCode.Ok);
+    type DataOfInterest = { id: string; requestCode: string };
+    const payload: UpdatePaymentRequestPayload = {
+      customer: "CUS_x1hp1dli4mdo1v0",
+      amount: 2_000_000,
+    };
+    const response = await client.paymentRequests.update(
+      (createResponse.data as DataOfInterest).requestCode,
+      payload,
+    );
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
+    assertEquals(response.message, "Payment request updated");
+  });
 
-  it("PaystackClient.paymentRequest.archive", () => {});
+  it("PaystackClient.paymentRequests.archive", async () => {
+    const createPayload: CreatePaymentRequestPayload = {
+      customer: "CUS_x1hp1dli4mdo1v0",
+      amount: 1000,
+    };
+    const createResponse = await client.paymentRequests.create(createPayload);
+    assertEquals(createResponse.statusCode, HttpStatusCode.Ok);
+    type DataOfInterest = { id: string; requestCode: string };
+    const response = await client.paymentRequests.archive(
+      (createResponse.data as DataOfInterest).requestCode,
+    );
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
+    assertEquals(response.message, 'Payment request has been archived');
+  });
 });
