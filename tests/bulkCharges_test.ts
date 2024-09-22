42a01fca047862abcb5ef11f52bf1f8ecab30802
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-22 12:27:21 +0100

    Test BulkChargeClient

--- tests/bulkCharges_test.ts
+++ tests/bulkCharges_test.ts
@@ -2,11 +2,8 @@ import { beforeAll, describe, it } from "@std/testing/bdd";
 import { PaystackClient } from "../mod.ts";
 import { load } from "@std/dotenv";
 import { BulkChargeInstruction } from "../src/types/clients/bulkCharge.ts";
-import { assertRejects } from "@std/assert/rejects";
 import { assertEquals } from "@std/assert/equals";
-import { assertInstanceOf } from "@std/assert/instance-of";
-import { PaystackClientError } from "../src/errors.ts";
-import { AxiosError } from "axios";
+import { HttpStatusCode } from "axios";
 
 describe("PaystackClient.bulkCharges", () => {
   let client: PaystackClient;
@@ -18,35 +15,66 @@ describe("PaystackClient.bulkCharges", () => {
 
   it("PaystackClient.bulkCharges.initiate", async () => {
     const payload: BulkChargeInstruction[] = [{
-      authorization: "",
+      authorization: "AUTH_w1renosr9o",
       amount: 1000,
       reference: "qwerty",
     }];
-    const err = await assertRejects(async () => {
-      await client.bulkCharges.initiate(payload);
-    });
-    assertInstanceOf(err, PaystackClientError);
-    assertEquals(err.status, 400);
+    const response = await client.bulkCharges.initiate(payload);
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
+    assertEquals(response.message, "Charges have been queued");
   });
 
   it("PaystackClient.bulkCharges.getBatches", async () => {
     const response = await client.bulkCharges.getBatches();
-    assertEquals(response.statusCode, 200);
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
     assertEquals(response.message, "Bulk charges retrieved");
   });
 
   it("PaystackClient.bulkCharges.getBatch", async () => {
     const response = await client.bulkCharges.getBatch("BCH_u5wt90cm016uy1b");
-    assertEquals(response.statusCode, 200);
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
     assertEquals(response.message, "Bulk charge retrieved");
   });
 
   it("PaystackClient.bulkCharges.getChargesInBatch", async () => {
-    const response = await client.bulkCharges.getChargesInBatch("BCH_u5wt90cm016uy1b");
-    assertEquals(response.statusCode, 200);
+    const response = await client.bulkCharges.getChargesInBatch(
+      "BCH_u5wt90cm016uy1b",
+    );
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
   });
 
-  it("PaystackClient.bulkCharges.pauseBatch", () => {});
+  it("PaystackClient.bulkCharges.pauseBatch", async () => {
+    const payload: BulkChargeInstruction[] = [{
+      authorization: "AUTH_w1renosr9o",
+      amount: 1000,
+      reference: "qwerty",
+    }];
+    const createResponse = await client.bulkCharges.initiate(payload);
+    assertEquals(createResponse.statusCode, HttpStatusCode.Ok);
+    type DataOfInterest = { batchCode: string };
+    const response = await client.bulkCharges.pauseBatch(
+      (createResponse.data as DataOfInterest).batchCode,
+    );
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
+    assertEquals(response.message, "Bulk charge batch has been paused");
+  });
 
-  it("PaystackClient.bulkCharges.resumeBatch", () => {});
+  it("PaystackClient.bulkCharges.resumeBatch", async () => {
+    const payload: BulkChargeInstruction[] = [{
+      authorization: "AUTH_w1renosr9o",
+      amount: 1000,
+      reference: "qwerty",
+    }];
+    const createResponse = await client.bulkCharges.initiate(payload);
+    assertEquals(createResponse.statusCode, HttpStatusCode.Ok);
+    type DataOfInterest = { batchCode: string };
+    const pauseResponse = await client.bulkCharges.pauseBatch(
+      (createResponse.data as DataOfInterest).batchCode,
+    );
+    assertEquals(pauseResponse.statusCode, HttpStatusCode.Ok);
+    const response = await client.bulkCharges.resumeBatch(
+      (createResponse.data as DataOfInterest).batchCode,
+    );
+    assertEquals(response.statusCode, HttpStatusCode.Ok);
+  });
 });
