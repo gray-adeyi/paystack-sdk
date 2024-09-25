4833aacc2d8e07475ef80fa317b621e58df33e3d
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-25 11:32:11 +0100

    Test TerminalClient

--- tests/terminals_test.ts
+++ tests/terminals_test.ts
@@ -1,27 +1,36 @@
 import { beforeAll, describe, it } from "@std/testing/bdd";
 import { PaystackClient } from "../mod.ts";
 import { load } from "@std/dotenv";
+import { assertEquals } from "@std/assert/equals";
+import { HttpStatusCode } from "axios";
 
+// A terminal is needed to test the TerminalClient
 describe("PaystackClient.terminals", () => {
   let client: PaystackClient;
 
   beforeAll(async () => {
-    await load({ envPath: "./.env", export: true });
+    await load({ envPath: "../.env", export: true });
+    client = new PaystackClient()
   });
 
-  it("PaystackClient.terminals.sendEvent", () => {});
+  it.skip("PaystackClient.terminals.sendEvent", async () => {
+  });
 
-  it("PaystackClient.terminals.getEventStatus", () => {});
+  it.skip("PaystackClient.terminals.getEventStatus", () => {});
 
-  it("PaystackClient.terminals.getTerminalStatus", () => {});
+  it.skip("PaystackClient.terminals.getTerminalStatus", () => {});
 
-  it("PaystackClient.terminals.getTerminals", () => {});
+  it("PaystackClient.terminals.getTerminals", async() => {
+    const response = await client.terminals.getTerminals()
+    assertEquals(response.statusCode, HttpStatusCode.Ok)
+    assertEquals(response.message, 'Terminals retrieved successfully')
+  });
 
-  it("PaystackClient.terminals.getTerminal", () => {});
+  it.skip("PaystackClient.terminals.getTerminal", () => {});
 
-  it("PaystackClient.terminals.updateTerminal", () => {});
+  it.skip("PaystackClient.terminals.updateTerminal", () => {});
 
-  it("PaystackClient.terminals.commissionTerminal", () => {});
+  it.skip("PaystackClient.terminals.commissionTerminal", () => {});
 
-  it("PaystackClient.terminals.decommissionTerminal", () => {});
+  it.skip("PaystackClient.terminals.decommissionTerminal", () => {});
 });
